78700db74844bbd4511695a0c5194513cf59811c
Henrique Veiga <henrique.messias@etec.sp.gov.br>
2024-09-23 00:48:24 -0300

    criacao da classe paciente, vetores e menu

---
+++ Paciente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    internal class Paciente
+    {
+        int p;
+        public string[] nome = new string[10];
+        public string[] data_nasc = new string[10];
+        public int[] idade = new int[10];
+        public string telefone;
+        public string[] cpf = new string[10];
+        public string[] tipo_sanguineo= new string[10];
+
+        public void Cadastro()
+        {
+            Console.WriteLine("Quantos pacientes deseja cadastrar:");
+            p = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o nome do Paciente:");
+            Console.WriteLine("Informe o CPF do Paciente:");
+            Console.WriteLine("Informe o tipo Sanguíneo:");
+            for (int i = 0; i < p; i++)
+            {
+                nome[i] = Console.ReadLine();
+                cpf[i] = Console.ReadLine();
+                tipo_sanguineo[i]= Console.ReadLine();
+            }
+
+        }
+        public void ListarPaciente()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (nome[i] != null || cpf[i] != null)
+                {
+                    Console.WriteLine("\n"+nome[i]);
+                    Console.WriteLine(cpf[i]);
+                    Console.WriteLine(tipo_sanguineo[i]+"\n\n");
+                }
+            }
+               
+                
+                
+            
+        }
+       
+                
+                   
+                
+           
+            
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string op;
+            bool continuar = true;
+            string resposta;
+           
+            int n;
+            
+            
+            Paciente p1 = new Paciente();
+            while (continuar)
+            {
+                Console.WriteLine("--Sistema Hospital--\n\n");
+                Console.WriteLine("O que Deseja Fazer?\n\n1-Cadastrar Paciente\n2-Listar Pacientes\nQ-Sair do Software");
+                op = Console.ReadLine();
+
+                switch (op)
+                {
+                    case "1":
+                        p1.Cadastro();
+                        break;
+
+                    case "2":
+
+                        p1.ListarPaciente();
+                        break;
+
+                    case "Q":
+                        Console.WriteLine("Saindo do Software em:");
+                        for (n = 3; n >= 0; n--)
+                        {
+                            Thread.Sleep(1200);
+                            Console.WriteLine(n);
+                        }
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.WriteLine("Deseja Continuar?\nS/N");
+                resposta = Console.ReadLine();
+                if (resposta == "N")
+                {
+                    continuar = false;
+                    Console.WriteLine("Saindo do Sistema!!");
+                    for (n = 3; n >= 0; n--)
+                    {
+                        Thread.Sleep(700);
+                        Console.WriteLine(n);
+                    }
+                    Environment.Exit(0);
+                }
+                    Console.WriteLine("/-----------------------------/");
+                Console.WriteLine("Voltando ao Sistema!!\n\nAguarde um Momento");
+                for (n = 3; n >= 0; n--)
+                {
+                    Thread.Sleep(650);
+                    Console.WriteLine(n);
+                }
+                    Console.WriteLine("*------------------------------*");
+                    
+                
+            }
+            
+           
+            Console.ReadKey();
+        }
+    }
+}
